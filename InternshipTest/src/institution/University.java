0edc17216ed2629d87ea6a43bda71f7efbfcf5cc
lav0lp3 <lav0lp3g00gl3@gmail.com>
2018-08-05 19:56:03 +0300

    Added methods for getting students medium knowledge level & for setting smart students to the internship

--- InternshipTest/src/institution/University.java
+++ InternshipTest/src/institution/University.java
@@ -28,4 +28,24 @@ public class University {
     public Internship getInternship() {
         return internship;
     }
+
+    private double getStudentKnowledgeMediumLevel() {
+        double medLevel;
+        double sum = 0;
+        for (Student student : students) {
+            sum += student.getKnowledge().getLevel();
+        }
+        medLevel = sum / students.size();
+        return medLevel;
+    }
+
+    public void setSmartStudents() {
+        List<Student> smartStudents = new ArrayList<>();
+        double medLevel = getStudentKnowledgeMediumLevel();
+        for (Student student : students) {
+            if (student.getKnowledge().getLevel() >= medLevel)
+                smartStudents.add(student);
+        }
+        internship.setStudents(smartStudents);
+    }
 }
--- InternshipTest/src/main/Application.java
+++ InternshipTest/src/main/Application.java
@@ -8,13 +8,13 @@ import person.consciousness.Knowledge;
 public class Application {
     public static void main(String[] args) {
         University university = new University("cH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko").withKnowledge(new Knowledge(5)));
+        university.addStudent(new Student("Andrew Kostenko").withKnowledge(new Knowledge(4)));
         university.addStudent(new Student("Julia Veselkina").withKnowledge(new Knowledge(1)));
         university.addStudent(new Student("Maria Perechrest").withKnowledge(new Knowledge(7)));
 
 
         Internship internship = university.getInternship();
-        //university.setSmartStudents();
+        university.setSmartStudents();
         System.out.println(university.getStudents());
         System.out.println(internship.getStudents());
     }
